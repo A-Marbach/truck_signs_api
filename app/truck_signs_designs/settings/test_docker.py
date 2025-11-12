a2da0a9976880be8eb7ee0e27472dda58e82c99e
Artur Marbach <Marbach.a@gmx.net>
2025-11-12 02:02:55 +0100

    remove .env

--- app/truck_signs_designs/settings/test_docker.py
+++ app/truck_signs_designs/settings/test_docker.py
@@ -1,40 +1,30 @@
-import environ
 from .base import *
+import os
 
-DEBUG = True
-
-env = environ.Env()
-# reading env file
-environ.Env.read_env()
-
-SECRET_KEY = env("DOCKER_SECRET_KEY")
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
+SECRET_KEY = os.environ.get("DOCKER_SECRET_KEY", "unsafe-secret-for-dev-only")
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DOCKER_DB_NAME'),
-        'USER': env('DOCKER_DB_USER'),
-        'PASSWORD': env('DOCKER_DB_PASSWORD'),
-        'HOST': env('DOCKER_DB_HOST'),
-        'PORT': env('DOCKER_DB_PORT'),
+        'NAME': os.environ.get("DOCKER_DB_NAME", "trucksigns"),
+        'USER': os.environ.get("DOCKER_DB_USER", "user"),
+        'PASSWORD': os.environ.get("DOCKER_DB_PASSWORD", "password"),
+        'HOST': os.environ.get("DOCKER_DB_HOST", "db"),
+        'PORT': int(os.environ.get("DOCKER_DB_PORT", 5432)),
     }
 }
 
-STRIPE_PUBLISHABLE_KEY=env("DOCKER_STRIPE_PUBLISHABLE_KEY")
-STRIPE_SECRET_KEY=env("DOCKER_STRIPE_SECRET_KEY")
-
-
+STRIPE_PUBLISHABLE_KEY = os.environ.get("DOCKER_STRIPE_PUBLISHABLE_KEY", "")
+STRIPE_SECRET_KEY = os.environ.get("DOCKER_STRIPE_SECRET_KEY", "")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("DOCKER_EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("DOCKER_EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("DOCKER_EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("DOCKER_EMAIL_HOST_PASSWORD", "")
